76f5fd709eaf6d178b2ba686828f0cc5775db0b1
Ruben Berreguero <rubenbr85@gmail.com>
2018-11-18 17:35:45 +0100

    Crear modulo home y about que se cargaran al iniciar la aplicacion (registrados en imports de app.module.ts)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { HomeModule } from './home/home.module';
+import { AboutModule } from './about/about.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { CoreModule } from './core/core.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CoreModule
+    CoreModule,
+    HomeModule,
+    AboutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
