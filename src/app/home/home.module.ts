6a8b1b8dead5c725b3e5610fca179f230c398357
Ruben Berreguero <rubenbr85@gmail.com>
2018-11-21 17:12:05 +0100

    Ejemplo de uso de FormsModule, creacion de componente home/saludo y home/referencias.

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
+import { SaludoComponent } from './saludo/saludo.component';
+import { FormsModule } from '@angular/forms';
+import { ReferenciasComponent } from './referencias/referencias.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, SaludoComponent, ReferenciasComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ]
 })
 export class HomeModule { }
---
+++ src/app/home/saludo/saludo.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'kc-saludo',
+  templateUrl: './saludo.component.html',
+  styleUrls: ['./saludo.component.css']
+})
+export class SaludoComponent implements OnInit {
+
+  usuario: string;
+  idioma: string;
+  constructor() { }
+
+  ngOnInit() {
+    this.usuario = 'Pepe';
+    this.idioma = 'es';
+  }
+
+  btnBorrar (ev) {
+    console.log(ev);
+    this.usuario = '';
+  }
+}
